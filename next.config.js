5a84ac60005157620183dcaf93baa4fab235bfbd
prediksi-del <kematianituada493@gmail.com>
2026-05-26 18:51:46 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true }, // Mengabaikan error linting saat build
-};
-module.exports = nextConfig;
