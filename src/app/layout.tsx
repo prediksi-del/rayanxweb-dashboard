98a3238bc2055ef699b087b5e3726a7b0c2f6447
prediksi-del <kematianituada493@gmail.com>
2026-05-26 07:54:19 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import LogicContainer from "@/components/LogicContainer";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <body>
+      <body className="bg-slate-950 text-slate-200">
         <AuthProvider>
           <LogicContainer>
             {children}
