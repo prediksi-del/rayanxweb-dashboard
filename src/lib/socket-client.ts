d783bfa4bf1c9fb0459e1bd7464dce93d345e444
prediksi-del <kematianituada493@gmail.com>
2026-05-26 18:02:35 +0700

    Delete src directory

--- src/lib/socket-client.ts
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-const URL = process.env.NEXT_PUBLIC_GATEWAY_URL || 'http://localhost:8080';
-
-export const socket = io(URL, {
-  autoConnect: false,
-  transports: ['websocket'],
-});
