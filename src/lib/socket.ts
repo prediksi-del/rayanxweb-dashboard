fd57fcf6266f87d4c0d4af2cb0f0abee241e8b31
prediksi-del <kematianituada493@gmail.com>
2026-05-26 07:19:03 +0700

    Delete src directory

--- src/lib/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-export const getSocket = (email: string, pin: string) => {
-  return io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {
-    auth: { email, token: pin },
-    transports: ["websocket"]
-  });
-};
